fa2c20ab5750ab14dcaa62a64cd4c282873fefa4
rom-dos <nicholasmatthewmorera@gmail.com>
2018-08-01 16:33:53 -0700

    countable

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,4 @@
+
 document.getElementById("heading").innerHTML =
   localStorage["title"] || "Enter title"; // default text
 document.getElementById("content").innerHTML = 
@@ -7,3 +8,8 @@ setInterval(function() {
   localStorage["title"] = document.getElementById("heading").innerHTML;
   localStorage["text"] = document.getElementById("content").innerHTML;
 })
+
+const area = document.getElementById('content');
+const callback = counter => document.getElementById("count").innerHTML = `Words: ${counter.words}`;
+
+Countable.on(area, callback);
